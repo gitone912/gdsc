1c5b4a00dae9c7f2eedfe657dadfa3e747af9f9a
AlaminBala <76796620+AlaminBala@users.noreply.github.com>
2023-04-04 05:39:50 +0100

    Homepage modified
    I have made the required modifications to the homepage

--- gsc/static/script.js
+++ gsc/static/script.js
@@ -1,24 +1,33 @@
 function calculateCarbon() {
-	let transportation = Number(document.getElementById("transportation").value);
-	let food = Number(document.getElementById("food").value);
-	let electricity = Number(document.getElementById("electricity").value);
-	let other = Number(document.getElementById("other").value);
+  let transportation = Number(document.getElementById("transportation").value);
+  let food = Number(document.getElementById("food").value);
+  let electricity = Number(document.getElementById("electricity").value);
+  let other = Number(document.getElementById("other").value);
 
-	let total = transportation + food + electricity + other;
-	let carbon = total * 0.5;
+  let total = transportation + food + electricity + other;
+  let carbon = total * 0.5;
 
-	document.getElementById("result").innerHTML = `Your carbon footprint is ${carbon} kg CO2e per year.`;
+  document.getElementById(
+    "result"
+  ).innerHTML = `Your carbon footprint is ${carbon} kg CO2e per year.`;
 }
 
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-	const signUpButton = document.getElementById('signUp');
-	const signInButton = document.getElementById('signIn');
-	const container = document.getElementById('container');
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
 
-	signUpButton.addEventListener('click', () => {
-	container.classList.add('right-panel-active');
-	});
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
 
-	signInButton.addEventListener('click', () => {
-	container.classList.remove('right-panel-active');
-	});
+const date = document.querySelector("date");
+
+const currentYear = new Date().getFullYear();
+
+document.addEventListener("load", () => {
+  date.innerHTML = `&copy; ${currentYear} Aniket`;
+});
